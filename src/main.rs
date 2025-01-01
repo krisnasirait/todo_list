b426446a31b51d2648327b38f14c0e1401f18f2f
Krisna Sirait <krisnasirait48@gmail.com>
2025-01-01 21:37:04 +0700

    added base function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+//struct task to store the Task data
+struct Task: {
+    id: Int,
+    description: String,
+    is_completed: bool
+}
+
 fn main() {
     println!("Hello, world!");
+}
+
+//fn to add task
+pub fn add() {
+
+}
+
+//fn to edit task
+pub fn edit() {
+
+}
+
+//fn to delete task
+pub fn delete() {
+
+}
+
+//fn to show main menu
+pub fn showMenu() {
+    println!("======SIMPLE TO DO LIST======");
+    println!("Build in RUST...by @krisnasirait");
+    println!("1. Add Task");
+    println!("2. Edit Task");
+    println!("3. Delete Task");
 }
